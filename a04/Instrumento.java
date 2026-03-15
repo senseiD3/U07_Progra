36556b771e2299524f277b89dcfdc40740d042b1
David Miguel <tuemail@ejemplo.com>
2026-03-15 12:39:40 +0100

    Comiteado

---
+++ a04/Instrumento.java
@@ -0,0 +1,30 @@
+package a04;
+
+public abstract class Instrumento {
+    Nota[] melodia;
+    int posicion;
+    
+    Instrumento(){
+        melodia = new Nota[10];
+        posicion = 0;
+    }
+    
+    public void add(Nota n) {
+        if (posicion < melodia.length) {
+            melodia[posicion] = n;
+            posicion++;
+        } else{
+            System.out.println("has alcanzado el maximo");
+        }
+    }
+    
+    abstract void interpretar();
+}
+
+
+/*@Override
+    void interpretar(){
+        for (int cont= 0; i < posicion; cont++) {
+            System.out.print(melodias[cont] + " ");
+        }
+    }*/
---
+++ a04/Nota.java
@@ -0,0 +1,3 @@
+package a04;
+
+public enum Nota{DO, RE, MI, FA, SOL, LA, SI}
